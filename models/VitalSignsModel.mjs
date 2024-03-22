d2658f1f537d86ae4e7cf9e28b2f3d6ae18a2d5e
Sebastian Silva <sebastiansilvabenites@gmail.com>
2024-03-21 22:51:35 -0400

    Add Temperature and Heart Rate Validation

--- models/VitalSignsModel.mjs
+++ models/VitalSignsModel.mjs
@@ -1,9 +1,18 @@
 import { Schema, model } from "mongoose";
 
 export const VitalSignsScheme = new Schema({
-    bodyTemperature: Number,
-    heartRate: Number,
-    bloodPressure: Number,
+    bodyTemperature: {
+        type: Number,
+        min: [30, 'Less than 30 is not allowed'],
+        max: [50, 'More than 50 is not allowed']
+    },
+    heartRate: {
+        type: Number,
+        min: 50,
+        max: 250
+    },
+    systolicBloodPresure: Number,
+    diastolicBloodPresure: Number,
     respirationRate: Number,
     weight: Number
 }, {timestamps: true});
