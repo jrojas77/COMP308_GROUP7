75198b1fca1e0c3644f1e8ecc23a2223d7c1bf0b
Sebastian Silva <sebastiansilvabenites@gmail.com>
2024-03-26 21:19:35 -0400

    Add Creation script

--- setup_backend.mjs
+++ setup_backend.mjs
@@ -1,8 +1,93 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { NurseModel } from "./models/NurseModel.mjs";
+import { PatientModel } from "./models/PatientModel.mjs";
 
 dotenv.config();
 
-const connection = await mongoose.connect(DB);
+let arrayOfNurses = [
+    {
+        email: "zo@cewaje.il",
+        password: "0319794786",
+        firstName: "Albert",
+        lastName: " Todd"
+    },
+    {
+        email: "ole@jic.tz",
+        password: "IAcgGnj8j6",
+        firstName: "Marion",
+        lastName: " Thompson"
+    },
+    {
+        email: "la@wivkeb.tk",
+        password: "OW2PnaQO1E",
+        firstName: "Kyle",
+        lastName: "Palmer"
+    },
+    {
+        email: "wow@jatiduwef.nu",
+        password: "P8NQYagJSG",
+        firstName: "Ronald",
+        lastName: "Doyle"
+    },
+    {
+        email: "la@eha.jm",
+        password: "UP15Y1eZws",
+        firstName: "Albert",
+        lastName: "Todd"
+    }
+];
 
-let arrayOfNurses = [];
+let arrayOfPatients = [
+    {
+        email: "efdibufi@ensekto.ni",
+        password: "CUEYUgCMjq",
+        firstName: "Virginia",
+        lastName: "Henry",
+        dailyInformation: [
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 }
+        ],
+        vitalSignsInformation: [
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 },
+            { bodyTemperature: 36, heartRate: 120, systolicBloodPresure: 30, diastolicBloodPresure: 25, respirationRate: 16, weight: 70 }
+        ]
+    },
+    {
+        email: "bewsoh@hisdirfi.mu",
+        password: "a5W8zsO8cp",
+        firstName: "Charlotte",
+        lastName: "Armstrong"
+    },
+    {
+        email: "cukfepte@nuvfov.ck",
+        password: "jz5vfQjkhg",
+        firstName: "Nathan",
+        lastName: "Daniel"
+    },
+    {
+        email: "golesrel@kopcon.vn",
+        password: "2774011338",
+        firstName: "Lora",
+        lastName: "Cain"
+    },
+];
+
+(async function () {
+    const connection = await mongoose.connect(process.env.DB);
+    await NurseModel.create(arrayOfNurses);
+    await PatientModel.create(arrayOfPatients);
+    mongoose.disconnect();
+})();
