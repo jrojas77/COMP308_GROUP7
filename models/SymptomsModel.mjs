bdff1bb39bf6d654c9412a3065d6d3752482dc18
Sebastian Silva <sebastiansilvabenites@gmail.com>
2024-04-09 22:24:51 -0400

    Changes in model

---
+++ models/SymptomsModel.mjs
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+
+export const SymptomsSchema = new Schema({
+    fever: {
+        type: Boolean,
+        default: false
+    },
+    tiredness: {
+        type: Boolean,
+        default: false
+    },
+    dryCough: {
+        type: Boolean,
+        default: false
+    },
+    difficultyInBreathing: {
+        type: Boolean,
+        default: false
+    },
+    soreThroat: {
+        type: Boolean,
+        default: false
+    },
+    pains: {
+        type: Boolean,
+        default: false
+    },
+    nasalCongestion: {
+        type: Boolean,
+        default: false
+    },
+    runnyNose: {
+        type: Boolean,
+        default: false
+    },
+    diarrhea: {
+        type: Boolean,
+        default: false
+    },
+    contact: {
+        type: String,
+        enum: ['no', 'yes', 'dont-know'],
+        default: 'no'
+    },
+    severity: {
+        type: String,
+        enum: ['none', 'mild', 'moderate', 'severe'],
+        defaut: 'none'
+    }
+}, { timestamps: true });
+
+export const SymptomsModel = model.Create
