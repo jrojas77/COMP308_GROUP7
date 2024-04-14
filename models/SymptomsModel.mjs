6b8c883e7ccf9f5e56c6efa1991dc475bd85be78
shield0225 <shieldsalcedo@yahoo.com>
2024-04-13 20:20:32 -0400

    added addSymptomsData mutator

--- models/SymptomsModel.mjs
+++ models/SymptomsModel.mjs
@@ -1,52 +1,55 @@
 import { Schema, model } from "mongoose";
 
-export const SymptomsSchema = new Schema({
+export const SymptomsSchema = new Schema(
+  {
     fever: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     tiredness: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     dryCough: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     difficultyInBreathing: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     soreThroat: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     pains: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     nasalCongestion: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     runnyNose: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     diarrhea: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     contact: {
-        type: String,
-        enum: ['no', 'yes', 'dont-know'],
-        default: 'no'
+      type: String,
+      enum: ["no", "yes", "dont-know"],
+      default: "no",
     },
     severity: {
-        type: String,
-        enum: ['none', 'mild', 'moderate', 'severe'],
-        defaut: 'none'
-    }
-}, { timestamps: true });
+      type: String,
+      enum: ["none", "mild", "moderate", "severe"],
+      defaut: "none",
+    },
+  },
+  { timestamps: true }
+);
 
-export const SymptomsModel = model.Create
+export const SymptomsModel = model("Symptoms", SymptomsSchema);
